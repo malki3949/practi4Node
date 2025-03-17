9969ae8bb10bb4bba9eb9f32b7440623b6d26a89
malki <s2043@gmail.com>
2025-03-17 17:35:07 +0200

    se

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-//  const express = require('express');
-// const app = express();
-// const port = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.get('/items', (req, res) => {
-//     res.json([{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }]);
-// });
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`);
-
-// });
-// import renderApi from '@api/render-api';
-
-// renderApi.auth('rnd_gyHrkkWBSF5ZXO94sCA5SCtZW20V');
-// renderApi.listServices({name: '', includePreviews: 'true', limit: '20'})
-//   .then(({ data }) => console.log(data))
-//   .catch(err => console.error(err));
-
-//  "rnd_gyHrkkWBSF5ZXO94sCA5SCtZW20V"
-// curl --request GET \
-//      --url 'https://api.render.com/v1/services?includePreviews=true&limit=20' \
-//      --header 'accept: application/json' \
-//      --header 'authorization: Bearer http://localhost:3000'
  import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
